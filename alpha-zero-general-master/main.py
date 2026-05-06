9eef5da57604d68aa02cfcd393530bcca7bc7e7f
AKIF-jk <i230583@isb.nu.edu.pk>
2026-05-07 00:30:38 +0500

    Update training parameters for improved performance in main.py

--- alpha-zero-general-master/main.py
+++ alpha-zero-general-master/main.py
@@ -9,13 +9,13 @@ log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 args = dotdict({
-    'numIters': 10,
-    'numEps': 25,              # Number of complete self-play games to simulate during a new iteration.
+    'numIters': 1000,
+    'numEps': 100,              # Number of complete self-play games to simulate during a new iteration.
     'tempThreshold': 15,        #
     'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
-    'numMCTSSims': 15,          # Number of games moves for MCTS to simulate.
-    'arenaCompare': 10,         # Number of games to play during arena play to determine if new net will be accepted.
+    'numMCTSSims': 25,          # Number of games moves for MCTS to simulate.
+    'arenaCompare': 40,         # Number of games to play during arena play to determine if new net will be accepted.
     'cpuct': 1,
 
     'checkpoint': './temp/',
@@ -24,7 +24,6 @@ args = dotdict({
     'numItersForTrainExamplesHistory': 20,
 
 })
-
 
 def main():
     log.info('Loading %s...', Game.__name__)
